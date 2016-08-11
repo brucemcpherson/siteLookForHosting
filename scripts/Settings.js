80bcd1de05f17b00315cc92c1ec1ecfd7fd6aa83
Bruce McPherson <bruce@mcpher.com>
2016-08-11 07:37:07 -0700

    updated by GasGit automation

--- scripts/Settings.js
+++ scripts/Settings.js
@@ -1,8 +1,6 @@
 var Settings = (function(ns) {
   
   ns.sites = {
-    useCache:true,                             // whether to use cache for folder maps
-    cacheSeconds:60*60,                        // how long to allow cache for
     search:"",                                 // enter a search term to concentrate on particular pages
     siteName:"share",                          // site name
     domain:"mcpher.com",                       // domain
@@ -11,10 +9,44 @@ var Settings = (function(ns) {
   };
   
   ns.report = {
-    sheetId:"1Ef4Ac5KkipxvhpcYCe9C_sx-TnD_kvV2E_a211wS6Po",  // sheet id to write to
-    sheetName:'site'                                         // sheetName to write to
+    sheetId:"19kXFMUh0DNTde_qtvsZ6FH4dAJ4YDCJhIJroLNT3vGY",         // sheet id to write to
+    sheetName:'site-' + ns.sites.domain + '-' + ns.sites.siteName,  // sheetName to write to
+    sheetHosting:'hosting-' + ns.sites.domain + '-' + ns.sites.siteName
   };
-   
+
+
+  
+  ns.identify = {
+    sheetId:ns.report.sheetId,                  // where to write the results
+    sheetFiles:"identify-" + ns.report.sheetHosting,
+    treatments: {
+      fileName:"{name}",                          // template for output file name - can be {id}-{name}
+      path:"/hosting/{mime}/",                    // template for file path  can be  {id}-{name}-{drivePath}-{mime}
+      failOnMissing:false,                        // whether to fail if file is missing
+      missingText:"MISSING"
+    } 
+  };
+                
+  ns.copy = {
+    sheetId:ns.report.sheetId,                  // where to write the summary
+    sheetCopied:"copy-" + ns.report.sheetHosting,
+    treatments: {
+      removePreviousVersions:true,
+      clearFolders:false
+    }
+  };
+    
+  ns.host = {
+    sheetId:ns.report.sheetId,
+    sheetName:'drive-' + ns.report.sheetHosting,
+    hostingType:'drive',
+    urls: {
+      drive:function(id) { 
+         return "https://drive.google.com/uc?id=" + id ;
+      }
+    }
+  };
+                
   return ns;
 })(Settings || {});
 
