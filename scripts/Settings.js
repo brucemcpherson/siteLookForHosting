d5782ac96f68d8606b784e3f23313562895fc670
Bruce McPherson <bruce@mcpher.com>
2016-08-18 03:47:45 -0700

    updated by GasGit automation

--- scripts/Settings.js
+++ scripts/Settings.js
@@ -21,7 +21,7 @@ var Settings = (function(ns) {
     sheetFiles:"identify-" + ns.report.sheetHosting,
     treatments: {
       fileName:"{name}",                          // template for output file name - can be {id}-{name}
-      path:"/hosting/{mime}/",                    // template for file path  can be  {id}-{name}-{drivePath}-{mime}
+      path:"/public/hosting/sites/xliberation/{mime}/",             // template for file path  can be  {id}-{name}-{drivePath}-{mime}
       failOnMissing:false,                        // whether to fail if file is missing
       missingText:"MISSING"
     } 
@@ -35,17 +35,71 @@ var Settings = (function(ns) {
       clearFolders:false
     }
   };
-    
+
+
   ns.host = {
     sheetId:ns.report.sheetId,
     sheetName:'drive-' + ns.report.sheetHosting,
-    hostingType:'drive',
+    hostingType:'gcs',
     urls: {
-      drive:function(id) { 
-         return "https://drive.google.com/uc?id=" + id ;
+      drive:function(id,path) { 
+        return "https://drive.google.com/uc?id=" + id ;
+      },
+      gcs:function (id,path) {
+        return "https://storage.googleapis.com/goinggas.com" + path;
       }
     }
   };
+
+  ns.update = {
+    testMode:{
+      active:true,
+      sampleRate:.01,
+      testStub:'rubbishfortesting',
+      deleteBefore:true,
+      reportDiffs:true
+    }
+  };
+
+ns.mimeShorts = function (mime) {
+  var shorts = {
+    "application/vnd.google-apps.script+json":"google",
+    "application/vnd.google-apps.script":"google",
+    "application/vnd.google-apps.folder":"google",
+    "application/vnd.google-apps.audio":"media",
+    "application/vnd.google-apps.document":"google",
+    "application/vnd.google-apps.drawing":"google",
+    "application/vnd.google-apps.file":"google",
+    "application/vnd.google-apps.form":"google",
+    "application/vnd.google-apps.photo":"media",
+    "application/vnd.google-apps.presentation":"google",
+    "application/vnd.google-apps.sites":"google",
+    "application/vnd.google-apps.fusiontable":"google",
+    "application/vnd.google-apps.spreadsheet":"google",
+    "application/vnd.google-apps.unknown":"google",
+    "application/vnd.google-apps.video":"media",
+    "application/xml":"xml",
+    "text/xml":"xml",
+    "application/json":"json",
+    "application/pdf":"pdf",
+    "image/png":"image",
+    "image/jpeg":"image",
+    "image/gif":"image",
+    "text/javascript":"js",
+    "text/css":"css",
+    "video/avi":"media",
+    "video/mpeg":"media",
+    "text/plain":"text",
+    "video/quicktime":"media",
+    "text/html":"markup",
+    "image/bmp":"image",
+    "image/x-icon":"image",
+    "image/tiff":"tiff"
+  };
+  mime = mime.toLowerCase();
+  return shorts[mime]  || mime;
+  
+};
                 
   return ns;
 })(Settings || {});
